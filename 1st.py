c6f8e0a116c707d4f85fdcbb7d0c8749f537c824
Ace Lawrence <clavanoace@gmail.com>
2021-12-09 19:09:20 -0800

    Incorporating instructions po for the program 2 naman

--- 1st.py
+++ 1st.py
@@ -12,7 +12,7 @@ while Rerun:
 
 
 
-# This will ask you for your lucky numbers should I call it and it 
+# This will ask you for your lucky numbers should I call it and it will record it in preperation of comparing it with the lottery digits...
     for digits in range(3):
         ThinkOfYourPick = int(input("Pick your lucky numbers: "))
         if 0 <= ThinkOfYourPick <= 9:
@@ -21,23 +21,25 @@ while Rerun:
             print ("Error!, it should be within the Min&Max range")
             ThinkOfYourPick = int(input("Pick your lucky numbers: "))
             YourChosenNumbers.append(ThinkOfYourPick)
-        SortEach = sorted(YourChosenNumbers)
+        # One additional information is this else statement executes if you pick numbers which are more than or less than the Min and Max
+        # numbers indicated for the lottery digits.
         
-
+# After entering your chosen lucky digits the program will now initialize the lottery digits that will be in comparison with your numbers......
     for luckydigits in range(3):
         while LuckyDigits in LotteryDigits:
             LuckyDigits = random.randint(Min, Max)
         LotteryDigits.append(LuckyDigits)
         
-
+# Then, this if else statements will evaluate the results on whether you won or lost.
     if ThinkOfYourPick in YourChosenNumbers == LotteryDigits:
         print ("Winner")
         print ("Your Numbers", YourChosenNumbers, "Vs, The Winning Digits", LotteryDigits)        
+        break
     else:
         print ("You loss")
         print ("Your Numbers", YourChosenNumbers, "Vs, The Winning Digits", LotteryDigits)        
 
-        
+# In the event of a loss, this will initialize and asks you if you would want to give another run with the game.
     InitializeRerun = input("Would you like to play again?: ")
     if InitializeRerun == 'Yes':
         print ("Here we go again")
